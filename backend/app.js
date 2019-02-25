ae62e11b26ea41b1bbaab3f58cc029a442488595
Udham Singh <udham.singh@quovantis.com>
2019-02-25 23:15:07 +0530

    Authorization header typo fixed

--- backend/app.js
+++ backend/app.js
@@ -28,8 +28,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept",
-        "Authorizaion"
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     res.setHeader(
         "Access-Control-Allow-Methods",
--- src/app/auth/auth-interceptor.ts
+++ src/app/auth/auth-interceptor.ts
@@ -4,13 +4,16 @@ import { AuthService } from './autth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         const authToken = this.authService.getToken();
-        const authRequest = req.clone({
-            headers: req.headers.set('Authorization', `Bearer ${authToken}`)
-        });
+        let authRequest = req;
+        if (authToken) {
+            authRequest = req.clone({
+                headers: req.headers.set('Authorization', `Bearer ${authToken}`)
+            });
+        }
         return next.handle(authRequest);
     }
 }
